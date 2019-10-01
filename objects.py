ec506a498e9820befc1d12d7d20a0e5e5cfdd15a
Paul Wrubel <pjwrubel@mtu.edu>
2019-10-01 00:20:01 -0500

    mostly refactoring, some gradiant tests

--- objects.py
+++ objects.py
@@ -6,19 +6,21 @@ from constants import *
 
 class Car:
     def __init__(self, label, config, location, speed = 0.0, acceleration = 1.0, speed_limit_variance = 0.0):
+        self.env = config.env
+
         self.label = label
         self.location = location
         self.speed = speed
         self.acceleration_constant = acceleration
         self.acceleration = self.acceleration_constant
         self.speed_limit_variance = speed_limit_variance
-        self.speed_limit = config.env["speed_limit"] + self.speed_limit_variance
+        self.confortable_speed_limit = self.env["speed_limit"] + self.speed_limit_variance
 
         self.car_length = 1.0
         self.car_width = 0.5
 
-        car_length_pixels = self.car_length * config.env["cars_to_pixels"]
-        car_width_pixels = self.car_width * config.env["cars_to_pixels"]
+        car_length_pixels = self.car_length * self.env["cars_to_pixels"]
+        car_width_pixels = self.car_width * self.env["cars_to_pixels"]
 
         self.left_brake_light_rect = Rect((car_length_pixels * 0.9, car_width_pixels * 0.75), (car_length_pixels * 0.1, car_width_pixels * 0.25))
         self.right_brake_light_rect = Rect((car_length_pixels * 0.9, 0), (car_length_pixels * 0.1, car_width_pixels * 0.25))
@@ -35,55 +37,91 @@ class Car:
         self.affected_surface = self.original_surface.copy()
 
     def update(self, config, dt):
-        self.location += 0.0001 * self.speed * dt
-        self.location %= config.env["road_length"]
+        self.location += self.speed * dt
+        self.location %= self.env["road_length"]
 
-        self.brakes = False
+        self.speed += (self.env["drag"] + self.acceleration) * dt
+        self.speed = max(self.speed, 0)
 
-        self.speed += 0.001 * self.acceleration * dt
-        if self.speed < 0:
-            self.speed = 0
-
-        if len(config.env["cars"]) > 1:
-            car_after = config.env["cars"][(config.env["cars"].index(self) + 1) % len(config.env["cars"])]
-            if car_after.location < self.location:
-                car_after_loc = car_after.location + config.env["road_length"]
-            else:
-                car_after_loc = car_after.location
-
-            diff = (car_after_loc - self.location) - 1
-
-            dist_coefficient = 0.3 # 0.2 + (self.speed / 15.0)
-
-            if diff >= dist_coefficient * 2:
-                if self.speed <= self.speed_limit:
-                    self.acceleration = self.acceleration_constant
-                else:
-                    self.acceleration = config.env["drag"]
-            # elif diff >= dist_coefficient * 1.5:
-            #     self.acceleration = config.coast_acceleration
-            elif diff >= dist_coefficient * 0.8:
-                self.acceleration = config.env["light_brakes_acceleration"]
-            else:
-                self.acceleration = config.env["heavy_brakes_acceleration"]
-
-            if diff <= 0:
-                print("Car " + str(self.label) + " CRASHED into Car " + str(car_after.label))
-                car_after.speed += self.speed * 0.9
-                car_after.location += 0.0002 * car_after.speed * dt
-                self.speed = 0
-
-        else:
-            if self.speed <= self.speed_limit:
-                self.acceleration = self.acceleration_constant
-            else:
-                self.acceleration = config.env["drag"]
+        self.acceleration, self.brakes = self.get_acceleration_and_brakes(dt)
+        self.handle_crash(dt)
 
         self.update_surface(config)
 
-    # def get_acceleration(self, config):
-        
+    def get_acceleration_and_brakes(self, dt):
+        next_car = self.car_after()
 
+        if next_car is not None:
+            # our front bumper to their rear bumper
+            distance_from_next_car = self.distance_from(next_car)
+            # positive if we are going faster
+            speed_difference = self.speed - next_car.speed
+
+            if distance_from_next_car < 2:
+                if speed_difference > 0:
+                    return self.env["max_braking_acceleration"], True
+                else:
+                    return self.env["max_braking_acceleration"] / 2.0, True
+
+            if self.speed >= self.confortable_speed_limit:
+                return self.confortable_speed_limit - self.speed, True
+
+            acceleration = distance_from_next_car - 1.0
+            return acceleration, acceleration < 0.0
+
+            # if distance_from_next_car >= 2:
+            #     if self.speed <= self.speed_limit:
+            #         return self.acceleration_constant, False
+            #     else:
+            #         return self.env["drag"], False
+            # # elif distance_from_next_car >= 1.5:
+            # #     return self.env["coast_acceleration"]
+            # elif distance_from_next_car >= 0.8:
+            #     return self.env["light_brakes_acceleration"], True
+            # else:
+            #     return self.env["heavy_brakes_acceleration"], True
+            
+        else: # if we are the only car
+            if self.speed <= self.speed_limit:
+                self.acceleration = self.acceleration_constant
+            else:
+                self.acceleration = self.env["drag"]
+
+    def handle_crash(self, dt):
+        next_car = self.car_after()
+        if next_car is not None and self.distance_from(next_car) <= 0:
+            print("Car " + str(self.label) + " CRASHED into Car " + str(next_car.label))
+            next_car_before_speed = next_car.speed
+            before_speed = self.speed
+
+            next_car.speed += (before_speed - next_car_before_speed) * 1.1
+            next_car.location += next_car.speed * dt * 2
+            self.location %= self.env["road_length"]
+
+            self.speed -= (before_speed - next_car_before_speed) * 0.9
+
+            return True
+        else:
+            return False
+
+    def car_after(self):
+        if len(self.env["cars"]) > 1:
+            my_index = self.env["cars"].index(self)
+            next_index = (my_index + 1) % len(self.env["cars"])
+            return self.env["cars"][next_index]
+        else:
+            return None
+
+    def distance_from(self, other_car):
+        if other_car is None:
+            return None
+        else:
+            if other_car.location < self.location:
+                other_car_loc = other_car.location + self.env["road_length"]
+            else:
+                other_car_loc = other_car.location
+
+            return other_car_loc - self.location - ((self.car_length / 2.0) + (other_car.car_length / 2.0))
 
     def update_surface(self, config):
         self.affected_surface = self.original_surface.copy()
